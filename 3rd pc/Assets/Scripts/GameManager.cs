290b85861abfbfe1053da77e69b8c7c13380d4a1
Kimchily <siji1117@gmail.com>
2021-02-14 03:00:42 +0900

    Unity(c#)
    유니티

---
+++ 3rd pc/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Boss boss;
+    public BossMissile bossmissile;
+    public MeleeArea bossmelee;
+
+    public PlayerController player;
+
+
+
+    static GameManager Instance;
+    public static GameManager GetInstance()
+    { return Instance; }
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+ 
+
+    private void Update()
+    {
+        ProcTarget();
+    }
+
+    void ProcTarget()
+    {
+
+        bossmissile.player = player.GetComponent<PlayerController>();
+        bossmelee.target = player.GetComponent<PlayerController>() ;
+        bossmissile.target = player.transform;
+    }
+}
