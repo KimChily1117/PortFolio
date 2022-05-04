cbceae4b0412b3961b808870a5cce70689c28c80
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2022-05-04 10:28:57 +0900

    test code
    비동기 , Photon 공부

---
+++ Multi_Thread_Test/Assets/Scripts/Thread.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Thread : MonoBehaviour
+{
+
+
+    public bool complete;
+    //async void testAsync()
+    //{
+    //    Task task = Task.Run(() =>
+    //    {
+    //        for (int i = 0; i < 999; i++)
+    //        {
+
+    //        }
+
+
+
+
+
+
+
+    //    });
+
+    //    await ;
+    //}
+
+
+
+}
+
+
