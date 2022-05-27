62b9fa7ca0767f692e701871d340b5add006b307
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2022-05-27 15:48:34 +0900

    UI작업중

--- photon_FPS/multi_fps/Assets/Scripts/UI/LobbyUI.cs
+++ photon_FPS/multi_fps/Assets/Scripts/UI/LobbyUI.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +11,29 @@ public class LobbyUI : MonoBehaviour
     [SerializeField]
     Dropdown Region_dropdown;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        
+        InitializedUI();
     }
+    
+    public void InitializedUI()
+    {
+        if (Region_dropdown == null) { return; } //추후 UI 자동화 코드 추가 예정
+
+        List<string> list = Enum.GetNames(typeof(Define.RegionType)).ToList();
+
+        Region_dropdown.ClearOptions();
+        Region_dropdown.AddOptions(list);
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 }
