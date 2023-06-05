53e862aaa395d0c8dd8df89118561fdc43ecd60e
KimChily1117 <siji1117@gmail.com>
2023-06-06 02:13:16 +0900

    23.06.06
    k

---
+++ C#/Server/ServerCore/Listener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket listenersocket;
+
+
+        public void Init(Socket socket, IPEndPoint iPEndPoint , )
+        {
+            listenersocket = socket;
+            
+        }
+    }
+}
