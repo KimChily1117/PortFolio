e89ed1023d11132b47b8bb5c15f618bed25471c1
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2023-09-06 23:14:07 +0900

    0906

--- C#/Project_Dawn/Assets/Scripts/04.Network/Packet/PacketHandler.cs
+++ C#/Project_Dawn/Assets/Scripts/04.Network/Packet/PacketHandler.cs
@@ -1,22 +1,23 @@
-﻿using DummyClient;
+﻿using Google.Protobuf;
+using Google.Protobuf.Protocol;
 using ServerCore;
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
 class PacketHandler
 {
-	public static void S_ChatHandler(PacketSession session, IPacket packet)
+	public static void S_ChatHandler(PacketSession session, IMessage packet)
 	{
 		S_Chat chatPacket = packet as S_Chat;
 		ServerSession serverSession = session as ServerSession;
 
+		Debug.Log(chatPacket.Context);
+	}
 
-        if (chatPacket.playerId == 1)
-            Debug.Log(chatPacket.chat);
-
-        //if (chatPacket.playerId == 1)
-        //Console.WriteLine(chatPacket.chat);
-    }
+	public static void S_EnterGameHandler(PacketSession session, IMessage packet)
+	{
+		S_EnterGame enterGamePacket = packet as S_EnterGame;
+		ServerSession serverSession = session as ServerSession;
+	}
 }
