690b79195b9acd765bf21306e07fcdf005a2c088
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2023-10-03 21:31:07 +0900

    알고리즘 공부 내용 추가
    10.03

--- C#/Server/Server/Algorithm/Program.cs
+++ C#/Server/Server/Algorithm/Program.cs
@@ -8,39 +8,41 @@ namespace Algorithm
         {
 
             Board board = new Board();
-            Player player = new Player();   
-            board.Initialize(25,player);
-            
-            
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            //board.BFSTest(0);
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
-            board.Render();
 
             int lastTick = 0;
+
             while (true)
             {
                 #region 프레임 관리
                 // 만약에 경과한 시간이 1/30초보다 작다면
                 int currentTick = System.Environment.TickCount;				
+                int deltaTime = currentTick - lastTick;
                 
-                
-                if (currentTick - lastTick < WAIT_TICK)
+                if (deltaTime < WAIT_TICK)
                     continue;
                 
                 lastTick = currentTick;
-                
+
                 #endregion
 
                 // 입력
 
                 // 로직
-               
 
 
-
+                player.Update(deltaTime);
+            
                 // 렌더링
                 Console.SetCursorPosition(0, 0);
+                board.Render();
                 
             }
         }
