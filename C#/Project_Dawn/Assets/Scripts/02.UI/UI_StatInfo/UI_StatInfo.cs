ded5fc3c1ae974d75a1cb4dcb7489deb1e991e26
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2024-05-10 18:29:55 +0900

    commit

---
+++ C#/Project_Dawn/Assets/Scripts/02.UI/UI_StatInfo/UI_StatInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_StatInfo : UI_PopUp
+{
+    private void Awake()
+    {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+
+
+    }
+}
