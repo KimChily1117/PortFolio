d46f1a590423c294800d1e8f87eeab5f431b9718
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2024-05-10 18:30:54 +0900

    작업최신화(24.05.10)

---
+++ C#/Server/Server/Server/Game/Room/GameRoom_Item.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf.Protocol;
+using Server.Game.Object;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game.Room
+{
+    public partial class GameRoom : JobSerializer
+    {
+
+        public void HandleEquipItem(Player player ,C_EquipItem euqipPacket)
+        {
+            if(player == null) 
+                return;
+
+            player.HandleEquipItem(euqipPacket); 
+        }
+
+    }
+}
