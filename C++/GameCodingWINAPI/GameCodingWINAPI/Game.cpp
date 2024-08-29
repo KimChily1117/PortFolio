7b2da1ca863e90cfec8c03d84574627d37044e88
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2024-08-29 21:03:42 +0900

    24.08.29
    WINAPI 개발시작

---
+++ C++/GameCodingWINAPI/GameCodingWINAPI/Game.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init(HWND hwnd)
+{
+	_hwnd = hwnd;
+
+	_hdc = ::GetDC(_hwnd);
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+	::Rectangle(_hdc, 200, 200, 400, 400);
+}
