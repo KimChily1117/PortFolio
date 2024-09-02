b72fe32a4a57a407ede81b99176608bac9435f88
Yeop_sun <sy.kim@newkl.net>
2024-09-03 02:18:02 +0900

    0902 SceneManager , Scene 구조설계 , 더블 버퍼링 적용

--- C++/GameCodingWINAPI/GameCodingWINAPI/DevScene.h
+++ C++/GameCodingWINAPI/GameCodingWINAPI/DevScene.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "Scene.h"
+
+
 class DevScene : public Scene
 {
 public:
+	DevScene();
+	~DevScene() override;
+
+public:
+
 	virtual void Init() override;
-
-
 	virtual void Update() override;
-
-
 	virtual void Render(HDC hdc) override;
 
+public:
+
+	Pos _playerPos = { 300,300 };
+	float _speed = 600;
 };
+
 
